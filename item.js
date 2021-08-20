4ddfafd53a143307cd07eb509e58c56dede171fa
sadjosarai <sadjosarai@gmail.com>
2021-08-20 22:44:43 +0100

    fin ajout, reste submission et quelque changement

--- item.js
+++ item.js
@@ -1,43 +1,48 @@
 var tag = document.getElementsByClassName("add");
 var tar = document.getElementById("cartTable");
 var cart= new Array();
-var d=["item","price","count"];
+var d=["item","price"];
 cart.push(d);
 var count=1;
-var iter=0;
+var iter = 0;
 function more(e){
     var r = e.parentNode.parentNode.parentNode.rowIndex;
-    console.log(r);
+    //console.log(r);
     var ele =tar.rows[r].cells[1].innerHTML;
-    console.log(parseInt(ele, 10));
+    //console.log(parseInt(ele, 10));
     var k=parseInt(ele, 10)+1;
-    console.log("this is k"+k);
+    //console.log("this is k : "+k);
     tar.rows[r].cells[1].innerHTML=k+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
+    var lastRowIndex=tar.rows.length - 1;
+    var total = parseInt(tar.rows[lastRowIndex].cells[1].innerHTML,10);
+    var prix = parseInt(cart[cart.length - r][1], 10);
+    tar.rows[lastRowIndex].cells[1].innerHTML = total + prix;
+    console.log("this is current total : " + total+" n "+prix+" n "+currentTotal);
 }
 function less(e){
     var r = e.parentNode.parentNode.parentNode.rowIndex;
-    console.log(r);
+    console.log("this is row index : "+r);
     var ele =tar.rows[r].cells[1].innerHTML;
-    console.log(parseInt(ele, 10));
     var k=parseInt(ele, 10)-1;
     if(k==0){
         tar.deleteRow(r);
+        console.log("cart avant splice : " + cart);
+        cart.splice(cart.length-r,1);
+        console.log("cart après splice :" + cart);
     }else{
-        console.log("this is k"+k);
+        console.log("this is k : "+k);
         tar.rows[r].cells[1].innerHTML=k+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
+        currentTotal = total - cart[cart.length - r][1];
     }
-   
 }
 function add(e) {
     var newrow = new Array();
     var i = e.rowIndex;
-    console.log(i);
     var item = e.cells[0].innerHTML;
     var price = e.cells[1].innerHTML;
     var check=false;
     newrow[0]=item;
     newrow[1]=price;
-    newrow[2]=count;
     for(i=0;i<=cart.length-1;i++){
         for(j=0;j<cart[i].length;j++)
             if(cart[i][j] === newrow[j]){
@@ -51,8 +56,6 @@ function add(e) {
     }
     if(check){
         console.log("already exist");
-        cart[index][2]+=1;
-        
     }else{
         cart.push(newrow);
         var row = tar.insertRow(1);
@@ -62,22 +65,17 @@ function add(e) {
         cell2.innerHTML=1+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
     }
     
-    function fill(tab){
-        var el=document.getElementsByClassName("total");
+    function fill(){
+        
         for(i=0;i<tab.length;i++){
             for(j=0;j<tab[i].length;j++){
-                let price=parseInt(tab[i][1], 10)*tab[i][2];
+                
                 iter+=price;
             }
         }
         console.log(iter);
-
     }
-   
-    
-   
-   /*  console.log(item);
-    console.log(price); */
     console.log(cart);
-    console.log(cart.length);
+    console.log("cart length : "+cart.length);
+    console.log();
 }
