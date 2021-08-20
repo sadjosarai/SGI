5316d6f77c16c15bb1368f52dd39b8edb590d5c4
sadjo L saraï <sarai.sadjo@facsciences-uy1.cm>
2021-08-20 09:53:32 +0100

    new change : adding to array

--- item.js
+++ item.js
@@ -1,11 +1,50 @@
 var tag = document.getElementsByClassName("add");
 var tar = document.getElementsByClassName("cartItems");
+var cart= new Array();
+var d=["item","price","count"];
+cart.push(d);
+var count=1;
+var iter=0;
 function add(e) {
+    var newrow = new Array();
     var i = e.rowIndex;
     console.log(i);
     var item = e.cells[0].innerHTML;
     var price = e.cells[1].innerHTML;
-    tar.innerHTML = tar.innerHTML+"<tr><td>" + item + "</td><td>" + price + "</td></tr>";
-    console.log(item);
-    console.log(price);
+    var check=false;
+    newrow[0]=item;
+    newrow[1]=price;
+    newrow[2]=count;
+    for(i=0;i<=cart.length-1;i++){
+        for(j=0;j<cart[i].length;j++)
+            if(cart[i][j] === newrow[j]){
+                check=true;
+                var index=i;
+            }else{
+                false;
+                break
+            }
+    }
+    if(check){
+        console.log("already exist");
+        cart[index][2]+=1;
+    }else{
+        cart.push(newrow);
+    }
+    function fill(tab){
+        var el=document.getElementsByClassName("total");
+        for(i=0;i<tab.length;i++){
+            for(j=0;j<tab[i].length;j++){
+                let price=parseInt(tab[i][1], 10)*tab[i][2];
+                iter+=price;
+            }
+        }
+        console.log(iter);
+
+    }
+   
+   /*  console.log(item);
+    console.log(price); */
+    console.log(cart);
+    console.log(cart.length);
 }
