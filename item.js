17467119417acd6930dc40074a380538de26ded0
sadjo L saraï <sarai.sadjo@facsciences-uy1.cm>
2021-08-21 10:29:49 +0100

    button add done, now just add a buying function

--- item.js
+++ item.js
@@ -7,17 +7,14 @@ var count=1;
 var iter = 0;
 function more(e){
     var r = e.parentNode.parentNode.parentNode.rowIndex;
-    //console.log(r);
     var ele =tar.rows[r].cells[1].innerHTML;
-    //console.log(parseInt(ele, 10));
     var k=parseInt(ele, 10)+1;
-    //console.log("this is k : "+k);
     tar.rows[r].cells[1].innerHTML=k+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
     var lastRowIndex=tar.rows.length - 1;
     var total = parseInt(tar.rows[lastRowIndex].cells[1].innerHTML,10);
     var prix = parseInt(cart[cart.length - r][1], 10);
-    tar.rows[lastRowIndex].cells[1].innerHTML = total + prix;
-    console.log("this is current total : " + total+" n "+prix+" n "+currentTotal);
+    var t=total + prix;
+    tar.rows[lastRowIndex].cells[1].innerHTML = t+"$";
 }
 function less(e){
     var r = e.parentNode.parentNode.parentNode.rowIndex;
@@ -26,13 +23,20 @@ function less(e){
     var k=parseInt(ele, 10)-1;
     if(k==0){
         tar.deleteRow(r);
-        console.log("cart avant splice : " + cart);
+        var lastRowIndex=tar.rows.length - 1;
+        var total = parseInt(tar.rows[lastRowIndex].cells[1].innerHTML,10);
+        var prix = parseInt(cart[cart.length - r][1], 10);
+        var t=total - prix;
+        tar.rows[lastRowIndex].cells[1].innerHTML = t+"$";
         cart.splice(cart.length-r,1);
-        console.log("cart après splice :" + cart);
     }else{
         console.log("this is k : "+k);
         tar.rows[r].cells[1].innerHTML=k+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
-        currentTotal = total - cart[cart.length - r][1];
+        var lastRowIndex=tar.rows.length - 1;
+        var total = parseInt(tar.rows[lastRowIndex].cells[1].innerHTML,10);
+        var prix = parseInt(cart[cart.length - r][1], 10);
+        var t=total - prix;
+        tar.rows[lastRowIndex].cells[1].innerHTML = t+"$";
     }
 }
 function add(e) {
@@ -63,6 +67,11 @@ function add(e) {
         var cell2=row.insertCell(1);
         cell1.innerHTML=e.cells[0].innerHTML;
         cell2.innerHTML=1+"<div class='arrow'><i class=\"fas fa-angle-up\" onclick=\"more(this)\"></i><i class=\"fas fa-angle-down\" onclick=\"less(this)\"></i></div>";
+        var lastRowIndex=tar.rows.length - 1;
+        var total = parseInt(tar.rows[lastRowIndex].cells[1].innerHTML,10);
+        var prix = parseInt(cart[cart.length - 1][1], 10);
+        var t=total + prix;
+        tar.rows[lastRowIndex].cells[1].innerHTML = t+"$";
     }
     
     function fill(){
